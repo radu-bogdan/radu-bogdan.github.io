825075bf4b28d05b777cb33ea1036afc75611b08
Bogdan Radu <radubogdan2@gmail.com>
2025-08-16 08:42:43 +0300

    reverted

--- primetals/assets/core-BKAC6fdO.js
+++
@@ -1,1 +0,0 @@
-import{g as e,a as o}from"./plotly-core-BAA7vXz2.js";var r=o();const t=e(r),c=Object.freeze(Object.defineProperty({__proto__:null,default:t},Symbol.toStringTag,{value:"Module"}));export{c};
--- primetals/assets/plotly-scatter-Cp3JySGL.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./plotly-core-BAA7vXz2.js";var r,e;function i(){return e||(e=1,r=t()),r}export{i as r};
